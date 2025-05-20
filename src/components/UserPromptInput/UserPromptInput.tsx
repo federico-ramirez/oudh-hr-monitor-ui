ae0bb2b549beb6453393a4e4441ab91c37c98130
federico-ramirez <00009220@uca.edu.sv>
2025-05-20 17:57:54 -0600

    feat(Home): Change Home page UI

--- src/components/UserPromptInput/UserPromptInput.tsx
+++ src/components/UserPromptInput/UserPromptInput.tsx
@@ -1,37 +1,44 @@
 export default function UserPromptInput() {
     return (
-        <div className="grid grid-rows-1 md:flex md:flex-row gap-2 m-auto py-4">
-            <div className='flex flex-col md:w-6/12 gap-2'>
-                <label htmlFor="" className="text-sm text-arsenic">
+        <div className="flex flex-col lg:flex-row lg:items-end gap-4 py-4 w-full mx-auto">
+            <div className="flex flex-col lg:w-6/12">
+                <label className="text-sm text-arsenic mb-1">
                     Seleccione una temática
                 </label>
                 <select
                     name="hr-topics"
                     id="hr-topics"
-                    className='h-12 px-4 bg-white rounded-xl focus:outline-indigo-900'
+                    className="h-12 px-4 bg-white rounded-xl focus:outline-indigo-900"
                     defaultValue=""
                 >
                     <option value="1">Derecho a la vida</option>
-                    <option value="1">Derecho a la salud</option>
+                    <option value="2">Derecho a la salud</option>
                 </select>
             </div>
-            <div className='flex flex-col md:w-4/12 gap-2'>
-                <label htmlFor="" className="text-sm text-arsenic">
-                    Seleccione una fecha
+            <div className="flex flex-col lg:w-2/12">
+                <label className="text-sm text-arsenic mb-1">
+                    Seleccione una fecha inicial
                 </label>
                 <input
-                    name="hr-topics"
-                    id="hr-topics"
-                    className='h-12 px-4 bg-white rounded-xl focus:outline-indigo-900'
-                    type='date'
-                >
-                </input>
+                    type="date"
+                    className="h-12 px-4 bg-white rounded-xl focus:outline-indigo-900"
+                />
             </div>
-            <div className='flex flex-col md:w-2/12 gap-2 items-center justify-end'>
-                <button className="bg-arsenic text-french-gray font-bold w-full h-12 rounded-xl hover:bg-davy-gray hover:duration-200">
+            <div className="flex flex-col lg:w-2/12">
+                <label className="text-sm text-arsenic mb-1">
+                    Seleccione una fecha final
+                </label>
+                <input
+                    type="date"
+                    className="h-12 px-4 bg-white rounded-xl focus:outline-indigo-900"
+                />
+            </div>
+            <div className="flex lg:w-2/12">
+                <button className="bg-arsenic text-french-gray font-bold w-full h-12 rounded-xl hover:bg-davy-gray transition duration-200 mt-6 md:mt-0">
                     Monitorear
                 </button>
             </div>
         </div>
+
     )
 }
